62370734c7d5d828a37cfae11cb5d709d0f5918c
6carol6 <mszxw999@gmail.com>
2016-02-29 20:55:21 +0800

    interesting

--- 268-Missing Number.cpp
+++ 268-Missing Number.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        
+        int ans = (0+nums[nums.size()-1])*(n+1)/2;
+        for(int i = 0; i < nums.size(); i++){
+            ans -= nums[i];
+        }
+        return ans;
     }
 };
