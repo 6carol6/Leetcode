95563780560c9abe2f3381bdd999fac68a5e461d
6carol6 <mszxw999@gmail.com>
2016-03-13 21:21:30 +0800

    pointer

---
+++ 328-Odd Even Linked List.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head == NULL || head->next == NULL) return head;
+        ListNode* oddh = head;
+        ListNode* evenh = head->next;
+        ListNode* oddp = head;
+        ListNode* evenp = evenh;
+        while(oddp->next != NULL && evenp != NULL){
+            oddp->next = evenp->next;
+            if(evenp->next != NULL){
+                oddp = oddp->next;
+                evenp->next = oddp->next;
+                if(evenp->next != NULL){
+                    evenp = evenp->next;
+                }
+            }
+        }
+        oddp->next = evenh;
+        return oddh;
+    }
+};
