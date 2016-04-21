5d9ab3c665cdcf31b461a92c7c23f8494cebcf3b
6carol6 <mszxw999@gmail.com>
2016-04-21 16:36:50 +0800

    math

---
+++ 343-Integer Break.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        if(n < 4) return n-1;
+        int ans = 1;
+        while(n>4){
+            ans *= 3;
+            n = n-3;
+        }
+        return n*ans;
+    }
+};
