7aede7c658872f88e89c0793487fbd64dbad5dfc
6carol6 <mszxw999@gmail.com>
2016-04-26 20:48:36 +0800

    a graceful way

--- 191-Number of 1 Bits.cpp
+++ 191-Number of 1 Bits.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
+		/*
+		//ans1:
         int ans = 0;
         while(n > 1){
             if(n%2 == 1) ans++;
@@ -8,5 +10,13 @@ public:
         }
         if(n == 1) ans++;
         return ans;
+		*/
+		//ans2:
+		int ans = 0;
+		while(n){
+			n = n&(n-1);
+			ans++;
+		}
+		return ans;
     }
 };
