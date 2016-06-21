d90fa6605aea1c39b446d064bdedca585300b810
6carol6 <mszxw999@gmail.com>
2016-06-21 21:29:18 +0800

    big data?

---
+++ 99-Plus One.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        vector<int> ans;
+        int cnt = 1;
+        for(int i = digits.size()-1; i >= 0; i--){
+            int tmp = digits[i]+cnt;
+            if(tmp == 10){
+                cnt = 1;
+                ans.push_back(0);
+            }else{
+                cnt = 0;
+                ans.push_back(tmp);
+            }
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
