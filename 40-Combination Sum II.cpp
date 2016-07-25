3e70d88ca6b5e3cfdda62d5bd5843296415f6cd9
6carol6 <mszxw999@gmail.com>
2016-07-25 16:35:22 +0800

    dfs

--- 40-Combination Sum II.cpp
+++ 40-Combination Sum II.cpp
@@ -1,25 +1,29 @@
+#include <algorithm>
 class Solution {
 private:
-    vector<int> solution;
-    void dfs(vector<vector<int>>& ans, int index, int sum, int num, int k, int n){
-        if(num > k || sum > n){
+    vector<int>  solution;
+    void dfs(vector<vector<int> >& ans, vector<int>& candidates, int target, int sum, int index){
+        if(sum == target){
+            if(find(ans.begin(), ans.end(), solution) == ans.end())
+                ans.push_back(solution);
+        }
+        else if(sum > target){
             return;
-        }else if(num == k && sum == n){
-            ans.push_back(solution);
         }else{
-            for(int i = index; i < 10; i++){
-                sum += i;
-                solution.push_back(i);
-                dfs(ans, i+1, sum, num+1, k, n);
+            for(int i = index; i < candidates.size(); i++){
+                solution.push_back(candidates[i]);
+                sum+=candidates[i];
+                dfs(ans, candidates, target, sum, i+1);
+                sum-=candidates[i];
                 solution.erase(solution.end()-1);
-                sum -= i;
             }
         }
     }
 public:
-    vector<vector<int>> combinationSum3(int k, int n) {
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
-        dfs(ans, 1, 0, 0, k, n);
+        sort(candidates.begin(), candidates.end());
+        dfs(ans, candidates, target, 0, 0);
         return ans;
     }
 };
